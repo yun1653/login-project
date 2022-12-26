d667592a515383b0f19c275403eb6f437113b212
yun1653 <yun1653@gmail.com>
2022-12-27 04:05:04 +0900

    4th

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 // 모듈
@@ -15,21 +14,3 @@ app.set('view engine', 'ejs');
 app.use('/', home);
 
 module.exports = app; // app를 내보내줘야 다른 모듈에서 이것을 받을 수 있다.
-=======
-'use strict';
-
-// 모듈
-const express = require('express');
-const app = express();
-
-// 라우팅
-const home = require('./routes/home/index');
-
-// 앱셋팅
-app.set('views', './views');
-app.set('view engine', 'ejs');
-
-app.use('/', home);
-
-module.exports = app; // app를 내보내줘야 다른 모듈에서 이것을 받을 수 있다.
->>>>>>> loginP
