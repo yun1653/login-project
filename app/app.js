422bbe4d553d8d7d4faab019e29b637c8a088c07
yun1653 <yun1653@gmail.com>
2022-12-27 05:22:56 +0900

    Tidy up folder structure

---
+++ app/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// 모듈
+const express = require('express');
+const app = express();
+
+// 라우팅
+const home = require('./src/routes/home/index');
+
+// 앱셋팅
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.use('/', home);
+
+module.exports = app; // app를 내보내줘야 다른 모듈에서 이것을 받을 수 있다.
