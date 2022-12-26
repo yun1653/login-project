58e8486f39558cdedf9b36e5ed51996a275db0f2
yun1653 <yun1653@gmail.com>
2022-12-27 06:22:59 +0900

    front 자바스크립트 파일 만들기

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ const home = require('./src/routes/home/index');
 // 앱셋팅
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use('/', home);
 
