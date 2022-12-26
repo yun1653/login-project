057577bfd3a89a526f06bc7526e78e04b21e4fb5
yun1653 <yun1653@gmail.com>
2022-12-27 01:18:52 +0900

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('여기는 루트입니다.');
+});
+
+app.get('/login', (req, res) => {
+  // "/login"에서 앞에 있는 "/"는 꼭 넣어줘라. "/"은 root를 의미하는 것이다.
+  res.send('여기는 로그인 화면입니다.');
+});
+
 app.listen(3000, () => {
   console.log('서버 가동');
 });
