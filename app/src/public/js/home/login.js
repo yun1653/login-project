449ed47973da49dbd5645d3c43d813b34886ceaf
yun1653 <yun1653@gmail.com>
2022-12-27 21:29:15 +0900

    saving the HTML object by DOM

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,18 @@
 'use strict';
-console.log('hello');
-console.log('bye');
+
+const id = document.querySelector('#id'),
+  psword = document.querySelector('#psword'),
+  loginBtn = document.querySelector('button');
+
+loginBtn.addEventListener('click', login);
+
+function login() {
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+  console.log(req);
+
+  // TODO: 이제 값을 서버에게 전달해 보자
+  //fetch();
+}
