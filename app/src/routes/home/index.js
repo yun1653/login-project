49589e20a847730ccda8be3e55198fce671e59a0
yun1653 <yun1653@gmail.com>
2022-12-29 03:14:07 +0900

    회원가입 페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,6 +7,7 @@ const ctrl = require('./home.ctrl');
 
 router.get('/', ctrl.output.home);
 router.get('/login', ctrl.output.login);
+router.get('/register', ctrl.output.register);
 router.post('/login', ctrl.process.login); // 브라우저(클라이언트)에서 오는 로그인 데이터를 서버에서 받는 API
 
 module.exports = router;
