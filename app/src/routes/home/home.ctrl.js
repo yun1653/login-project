714a40483c5050feff64b8f947becb69be26fe82
yun1653 <yun1653@gmail.com>
2022-12-29 01:10:35 +0900

    User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const UserStorage = require('../../models/UserStorage');
+const User = require('../../models/User');
 
 const output = {
   home: (req, res) => {
@@ -14,25 +14,8 @@ const output = {
 
 const process = {
   login: (req, res) => {
-    const id = req.body.id;
-    const psword = req.body.psword;
-
-    // const userStorage = new UserStorage(); // static 를 사용하지 않았을 때
-    //console.log(userStorage.users);
-
-    const users = UserStorage.getUsers('id', 'psword');
-
-    const response = {};
-    if (users.id.includes(id)) {
-      const idx = users.id.indexOf(id);
-      if (users.psword[idx] === psword) {
-        response.success = true;
-        return res.json(response);
-      }
-    }
-
-    response.success = false;
-    response.msg = '로그인에 실패했습니다.';
+    const user = new User(req.body);
+    const response = user.login();
     return res.json(response);
   },
 };
