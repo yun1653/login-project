309f2ad5e972378852e6cf95dbdf4bb680d2973f
yun1653 <yun1653@gmail.com>
2023-01-05 02:23:40 +0900

    벡엔드 맛보기 로그인 프로젝트 최종 최적화

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -24,32 +24,44 @@ const process = {
   login: async (req, res) => {
     const user = new User(req.body);
     const response = await user.login();
-    if (response.err) {
-      logger.error(
-        `POST / login 200 Response: "success: ${response.success}, ${response.err}"`
-      );
-    } else {
-      logger.info(
-        `POST / login 200 Response: "success: ${response.success}, msg: ${response.msg}"`
-      );
-    }
-    return res.json(response);
+
+    const url = {
+      method: 'POST',
+      path: '/login',
+      status: response.err ? 400 : 200, //200은 서버에서 정상응답해줬단 의미
+    };
+
+    log(response, url);
+    return res.status(url.status).json(response);
   },
 
   register: async (req, res) => {
     const user = new User(req.body);
     const response = await user.register();
-    if (response.err) {
-      logger.error(
-        `POST / register 200 Response: "success: ${response.success}, ${response.err}"`
-      );
-    } else {
-      logger.info(
-        `POST / register 200 Response: "success: ${response.success}, msg: ${response.msg}"`
-      );
-    }
-    return res.json(response);
+
+    const url = {
+      method: 'POST',
+      path: '/register',
+      status: response.err ? 409 : 201,
+    };
+
+    log(response, url);
+    return res.status(url.status).json(response);
   },
 };
 
 module.exports = { output, process };
+
+const log = (response, url) => {
+  if (response.err) {
+    logger.error(
+      `${url.method} ${url.path} ${url.status} Response: ${response.success}, ${response.err}`
+    );
+  } else {
+    logger.info(
+      `${url.method} ${url.path} ${url.status} Response: ${response.success}, ${
+        response.msg || ''
+      }`
+    );
+  }
+};
